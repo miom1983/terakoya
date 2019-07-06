5b5e10b1595c96fa77ec6f094c6341dc3d449b13
Your Name <you@example.com>
2019-07-06 22:07:12 +0900

    FizzBuzz2回目プッシュ

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,13 +8,14 @@
 
 #■ 回答 
 (1..100).each do |number|
-if number % 3 == 0 && number % 5 == 0
+if number % 15 == 0
     puts "FizzBuzz"
 elsif number % 3 == 0
     puts "Fizz"
 elsif number % 5 == 0
     puts "Buzz"
-else puts number
+else
+    puts number
 end
 end
 
